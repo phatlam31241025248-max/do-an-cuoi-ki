b18362f2632ba940889420373f85e40868c57cec
phatlam31241025248-max <phatlam.31241025248@st.ueh.edu.vn>
2026-04-17 13:39:25 +0700

    Delete views directory

--- views/components/alert.php
+++
@@ -1,6 +0,0 @@
-<?php if ($message = flash('success')): ?>
-    <div class="alert alert-success border-0 shadow-soft"><?= e($message) ?></div>
-<?php endif; ?>
-<?php if ($message = flash('error')): ?>
-    <div class="alert alert-danger border-0 shadow-soft"><?= e($message) ?></div>
-<?php endif; ?>
